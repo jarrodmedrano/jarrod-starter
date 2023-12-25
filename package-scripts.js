bf131dd2848a8a33580f4791740e19b5457a0877
Jarrod Medrano <jmedran@gmail.com>
2023-12-25 14:26:27 -0700

    feat: init commit

---
+++ package-scripts.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+const clientPath = path.resolve(__dirname, 'apps/client')
+const apiPath = path.resolve(__dirname, 'apps/api')
+
+const ciApiPath = path.resolve(__dirname, 'out/apps/api')
+const ciWebPath = path.resolve(__dirname, 'out/apps/web')
+
+module.exports = {
+  scripts: {
+    prepare: {
+      default: 'nps prepare.install prepare.format',
+      install: 'npx husky install && pnpm install',
+    },
+    build: {
+      default: 'npx turbo run build',
+    },
+    eslint: {
+      default: 'eslint "**/*.+(js|jsx|ts|tsx)"',
+      fix: 'eslint --fix "**/*.+(js|jsx|ts|tsx)"',
+    },
+    prettier: {
+      default: 'prettier --check "**/*.+(js|jsx|ts|tsx|json|yml|yaml|md|css)"',
+      fix: 'prettier --write "**/*.+(js|jsx|ts|tsx|json|yml|yaml|md|css)"',
+    },
+    lintStaged: {
+      default: 'nps prettier.fix eslint.fix',
+    },
+    precommit: {
+      default: 'nps lintStaged',
+    },
+    prebuild: {
+      default: 'nps lintStaged',
+    },
+  },
+}
