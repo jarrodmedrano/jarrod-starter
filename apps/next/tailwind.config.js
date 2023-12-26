130ba004f7997318ee1c641a5f410a06c27e40c0
Jarrod Medrano <jmedran@gmail.com>
2023-12-26 11:17:27 -0700

    feat: added ui package and tw components

--- apps/next/tailwind.config.js
+++ apps/next/tailwind.config.js
@@ -11,4 +11,5 @@ module.exports = {
   theme: {
     ...theme,
   },
+  extends: 'ui/tailwind.config',
 }
--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -8,4 +8,5 @@ module.exports = {
   theme: {
     ...theme,
   },
+  extends: '../ui/tailwind.config',
 }
