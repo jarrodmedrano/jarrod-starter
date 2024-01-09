b10256ccd4e1aa5576ef42ec63cb4368c46b6c22
Jarrod Medrano <jmedran@gmail.com>
2024-01-09 06:28:21 -0600

    feat: add email signin

--- apps/next/app/api/auth/[...nextauth]/route.ts
+++ apps/next/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,41 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
-// import PostgresAdapter from '@auth/pg-adapter'
-// import { Pool } from 'pg'
+import AppleProvider from 'next-auth/providers/apple'
+import EmailProvider from 'next-auth/providers/email'
+import PostgresAdapter from '@auth/pg-adapter'
+import { Pool } from 'pg'
+import type { Adapter } from 'next-auth/adapters'
 
-// const pool = new Pool({
-//   host: 'localhost',
-//   user: 'database-user',
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000,
-// })
+const pool = new Pool({
+  host: 'localhost',
+  user: 'database-user',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
-const authOptions = {
-  // adapter: PostgresAdapter(pool),
+const authOptions: NextAuthOptions = {
+  adapter: PostgresAdapter(pool) as Adapter,
 
   // Configure one or more authentication providers
   providers: [
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST || 'localhost',
+        port: process.env.EMAIL_SERVER_PORT || 587,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER || '',
+          pass: process.env.EMAIL_SERVER_PASSWORD || '',
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_ID || '',
+      clientSecret: process.env.APPLE_SECRET || '',
+    }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID || '',
       clientSecret: process.env.FACEBOOK_SECRET || '',
