7418ab3dd370e2cd3c84d9af5288a2b0e6ce2f0e
jarrodmedrano <jmedran@gmail.com>
2024-06-03 12:43:00 -0500

    feat: added clack for initializing the project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import * as p from '@clack/prompts';
+import color from 'picocolors';
+import { setTimeout } from 'node:timers/promises';
+
 
 const path = require('path');
 const fs = require('fs-extra');
@@ -7,10 +11,77 @@ const { execSync } = require('child_process');
 const templateDir = path.join(__dirname, '../template');
 const targetDir = process.cwd();
 
-fs.copy(templateDir, targetDir)
-  .then(() => {
-    console.log('Project created successfully.');
-  })
-  .catch((err: any) => {
-    console.error('Error creating project:', err);
-  });
+
+async function main() {
+	console.clear();
+
+	await setTimeout(1000);
+
+	p.intro(`${color.bgCyan(color.black(' create-app '))}`);
+
+  const copyFiles = async () => {
+    await fs.copy(templateDir, targetDir)
+    .then(() => {
+      console.log('Project created successfully.');
+    })
+    .catch((err: any) => {
+      console.error('Error creating project:', err);
+    })
+  }
+
+	const project = await p.group(
+		{
+			path: () =>
+				p.text({
+					message: 'Where should we create your project?',
+					placeholder: './sparkling-solid',
+					validate: (value: any) => {
+						if (!value) return 'Please enter a path.';
+						if (value[0] !== '.') return 'Please enter a relative path.';
+					},
+				}),
+			database: ({ results }: {
+        results: any;
+      }) =>
+				p.select({
+					message: `Pick a database type within "${results.path}"`,
+					initialValue: 'psql',
+					maxItems: 1,
+					options: [
+            { value: 'psql', label: 'Postgresql' },
+            { value: 'sqlite', label: 'SQLite' },
+          ],
+				}),
+			install: () =>
+				p.confirm({
+					message: 'Install dependencies?',
+					initialValue: false,
+				}),
+		},
+		{
+			onCancel: () => {
+				p.cancel('Operation cancelled.');
+				process.exit(0);
+			},
+		}
+	);
+
+	if (project.install) {
+		const s = p.spinner();
+		s.start('Installing via pnpm');
+		await setTimeout(2500);
+		s.stop('Installed via pnpm');
+	}
+
+  await copyFiles();
+
+	let nextSteps = `cd ${project.path}        \n${project.install ? '' : 'pnpm install\n'}pnpm dev`;
+
+	p.note(nextSteps, 'Next steps.');
+
+	p.outro(`Problems? ${color.underline(color.cyan('https://example.com/issues'))}`);
+}
+
+main().catch(console.error);
+
+
