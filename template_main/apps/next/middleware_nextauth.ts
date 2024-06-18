1f918d5074142d257863336e609ae226fc5495ef
Jarrod Medrano <jmedran@gmail.com>
2024-06-18 15:02:12 -0500

    corrected next auth

--- template_main/apps/next/middleware_nextauth.ts
+++ template_main/apps/next/middleware_nextauth.ts
@@ -1,7 +1,103 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+// import NextAuth from 'next-auth'
+// import { NextResponse } from 'next/server'
 
-export default clerkMiddleware()
+import { NextAuthRequest } from 'next-auth/lib'
+import { NextResponse } from 'next/server'
 
+// import { authConfig } from './auth'
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  privateRoutes,
+} from './routes'
+// const { auth } = NextAuth(authConfig)
+
+// import { auth } from './auth'
+// import { NextAuthRequest } from 'next-auth/lib'
+
+// const { auth } = NextAuth(authConfig)
+
+// export default auth((req: NextAuthRequest): Response | Promise<Response> => {
+//   const { nextUrl } = req
+//   const isLoggedIn = !!req.auth
+
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+//   if (isApiAuthRoute) {
+//     return NextResponse.next()
+//   }
+
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+//     }
+//     return NextResponse.next()
+//   }
+
+//   if (!isLoggedIn && !isPublicRoute) {
+//     let callbackUrl = nextUrl.pathname
+//     if (nextUrl.search) {
+//       callbackUrl += nextUrl.search
+//     }
+
+//     const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+//     return Response.redirect(
+//       new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+//     )
+//   }
+
+//   return NextResponse.next()
+// })
+export default async function middleware(
+  req: NextAuthRequest,
+): Promise<Response> {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  // const _isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname)
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return NextResponse.next()
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+    return Response.redirect(
+      new URL(`/signin?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    )
+  }
+
+  return NextResponse.next()
+}
+// Optionally, don't invoke Middleware on some paths
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// }
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
