9d888f0239be5adf5e35f7efe2291511cb45a9d3
Jarrod Medrano <jmedran@gmail.com>
2024-07-01 10:35:06 -0500

    fixed mobile login button

--- template_main/packages/ui/components/header/header.tsx
+++ template_main/packages/ui/components/header/header.tsx
@@ -98,12 +98,12 @@ export const Header = ({
                 ))}
               </div>
               <div className="py-6">
-                <a
-                  href="/signin"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                >
-                  Log in
-                </a>
+                <LoginButton
+                  companyLink={companyLink}
+                  companyName={companyName}
+                  navigation={navigation}
+                  companyLogo={companyLogo}
+                />
               </div>
             </div>
           </div>
