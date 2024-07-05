81bd6b905cb8173178ead9293cd4c1149409bb8e
Jarrod Medrano <jmedran@gmail.com>
2024-07-05 09:12:20 -0500

    fixed create slug page

--- template_main/apps/next/app/create/[slug]/page.tsx
+++ template_main/apps/next/app/create/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import withAuth from '../../utils/withAuth'
 import CreateScreen from '@app/features/create/screen'
+import listEntitiesForUser from '../../../actions/entities/listEntitiesforUser'
+
 const CreatePage = (props: any) => {
-  return <CreateScreen {...props} />
+  return <CreateScreen {...props} listEntitiesForUser={listEntitiesForUser} />
 }
 
 export default withAuth(CreatePage)
