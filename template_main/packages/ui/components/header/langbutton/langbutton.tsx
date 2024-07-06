dd141dc86e954b625e33a5698ddaeb6ff65127b4
Jarrod Medrano <jmedran@gmail.com>
2024-07-06 15:22:58 -0500

    fix button color and add to internal header

--- template_main/packages/ui/components/header/langbutton/langbutton.tsx
+++ template_main/packages/ui/components/header/langbutton/langbutton.tsx
@@ -17,7 +17,7 @@ export function LangButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="w-9 px-0">
-          <LanguageIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <LanguageIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
           <span className="sr-only">Toggle language</span>
         </Button>
       </DropdownMenuTrigger>
