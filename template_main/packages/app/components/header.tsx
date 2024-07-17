e1b38b100d49b88173998d1a6357eadd5bf09923
Jarrod Medrano <jmedran@gmail.com>
2024-07-17 08:18:29 -0500

    refactored header nav

--- template_main/packages/app/components/header.tsx
+++ template_main/packages/app/components/header.tsx
@@ -1,11 +1,51 @@
 import { Header as HeaderNav } from '@ui/components/header/header'
 import { Logo } from '@ui/components/icons/logo'
-import { navigation } from '../utils/constants'
+import React, { useEffect } from 'react'
+import { createKeyIndex } from '../../ui/components/dashboard/utils/build-nav'
+import { sitemap } from '../../sitemap/sitemap.json'
 
 export const Header = () => {
+  const [mainNav, setMainNav] = React.useState<
+    {
+      name: string
+      href: string
+    }[]
+  >([])
+
+  useEffect(() => {
+    const keyIndex = createKeyIndex(sitemap)
+    const pages = keyIndex.pages
+    pages.shift()
+    const navs: {
+      name: string
+      href: string
+    }[] = pages.reduce((prev: any, curr: any) => {
+      return curr.name !== 'about'
+        ? [
+            ...prev,
+            {
+              name: curr.name,
+              href: curr.loc,
+            },
+          ]
+        : [
+            ...prev,
+            ...curr.children.map((child: any) => {
+              return {
+                name: child.page.name,
+                href: child.page.loc,
+              }
+            }),
+          ]
+    }, [])
+    navs.pop()
+    navs.pop()
+    setMainNav(navs)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <HeaderNav
-      navigation={navigation}
+      navigation={mainNav}
       companyName={process.env.COMPANY_NAME || 'Company Name'}
       companyLogo={<Logo className="h-8 w-auto" />}
       companyLink={'/'}
