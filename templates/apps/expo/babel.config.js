031793f7ad602ebe4a111cef2fe82577964ec885
Jarrod Medrano <jmedran@gmail.com>
2024-10-27 13:30:34 -0500

    expo testing

--- templates/apps/expo/babel.config.js
+++ templates/apps/expo/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
-    plugins: ['react-native-reanimated/plugin', 'expo-router/babel'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      'nativewind/babel',
+      'expo-router/babel',
+    ],
   }
 }
